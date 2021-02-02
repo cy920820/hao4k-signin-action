e969d1f5ac63550a2ad20bdf621d7ec5564dee56
cy920820 <cuiyang673308817@gmail.com>
2021-02-02 12:10:15 +0800

    feat(signin.py): 增加 server 酱告警通知

--- SignIn.py
+++ SignIn.py
@@ -2,9 +2,15 @@ import requests
 import os
 import re
 
+# hao4k 账户信息
 username = os.environ["HAO4K_USERNAME"]
 password = os.environ["HAO4K_PASSWORD"]
+# 添加 server 酱通知
+sckey = os.environ["ServerChan_SCKEY"]
+send_url = "https://sc.ftqq.com/%s.send" % (sckey)
+send_content = 'Server ERROR'
 
+# hao4k 签到 url
 user_url = "https://www.hao4k.cn/member.php?mod=logging&action=login&phonelogin=no"
 base_url = "https://www.hao4k.cn/"
 signin_url = "https://www.hao4k.cn/plugin.php?id=k_misign:sign&operation=qiandao&formhash={formhash}&format=empty"
@@ -17,7 +23,6 @@ form_data = {
     'answer': ""
 }
 inajax = '&inajax=1'
-
 
 def run(form_data):
     s = requests.Session()
@@ -36,21 +41,26 @@ def run(form_data):
     login_resp = s.post(login_url, data=form_data)
     test_resp = s.get('https://www.hao4k.cn/k_misign-sign.html')
     if username in test_resp.text:
-        print('login!')
+      print('login!')
     else:
-        return 'login failed!'
+      return 'login failed!'
     signin_text = re.search('formhash=(.*?)"', test_resp.text)
     signin_resp = s.get(signin_url.format(formhash=signin_text.group(1)))
     test_resp = s.get('https://www.hao4k.cn/k_misign-sign.html')
     if '您的签到排名' in test_resp.text:
-        print('signin!')
+      print('signin!')
     else:
-        return 'signin failed!'
+      return 'signin failed!'
 
 
 if __name__ == "__main__":
-    signin_log = run(form_data)
-    if signin_log is None:
-        print('Sign in automatically!')
-    else:
-        print(signin_log)
+  signin_log = run(form_data)
+  if signin_log is None:
+    send_content = "hao4k 每日签到成功！"
+    print('Sign in automatically!')
+  else:
+    send_content = signin_log
+    print(signin_log)
+  params = {'text': 'hao4k 每日签到结果通知：', 'desp': send_content}
+  requests.post(send_url, params=params)
+  print('已通知 server 酱')
